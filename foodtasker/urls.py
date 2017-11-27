1c4f4ab5521632f4902ef553b057842193ae0405
Mark Milan <markgnome@gmail.com>
2017-11-28 07:40:21 +0800

    add bootstrap

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from foodtaskerapp import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),

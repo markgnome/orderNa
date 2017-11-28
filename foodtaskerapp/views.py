5fcd4af258ddd3b4a6c51597d73a1495c03d4578
Mark Milan <markgnome@gmail.com>
2017-11-28 16:03:21 +0800

    create sign up form

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from foodtaskerapp.forms import UserForm, RestaurantForm
+
 # Create your views here.
 
 def home(request):
@@ -8,3 +11,11 @@ def home(request):
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
     return render(request, 'restaurant/index.html', {})
+
+def restaurant_sign_up (request):
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+    return render(request, 'restaurant/sign_up.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
